c6c61a661bcb69b429473c36a2145442a9533262
rohit-iitm <119483801+rohit-iitm@users.noreply.github.com>
2022-11-30 15:55:44 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title('GA 8')
 st.subheader('Check if number is ODD or Even')
 placeholder_number = st.empty()
-n = placeholder_dividend.number_input('Enter your number')
+n = placeholder_number.number_input('Enter your number')
 if float(n)%2 == 0:
 	st.write('EVEN Number')
   else:
